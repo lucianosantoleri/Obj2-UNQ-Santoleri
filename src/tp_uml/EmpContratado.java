737bb9820dd0a4611e1e38bb1af4d6ff9484c115
Luciano Santoleri <luchosantoleri@gmail.com>
2021-04-22 08:56:31 -0300

    terminando clases en tp uml

--- src/tp_uml/EmpContratado.java
+++ src/tp_uml/EmpContratado.java
@@ -1,5 +1,32 @@
 package tp_uml;
 
+import java.util.Date;
+
 public class EmpContratado extends Empleado {
+	
+	private int numeroContrato;
+	private String medioDePago;
+
+	public EmpContratado(String nombre, String direccion, String estadoCivil, Date fechaNac, Float sueldoBasico,
+			             int nroContrato, String medioDePago) {
+		super(nombre, direccion, estadoCivil, fechaNac, sueldoBasico);
+		this.numeroContrato = nroContrato;
+		this.medioDePago = medioDePago;
+	}
+
+	@Override
+	public Float calculoDeSueldoBruto() {
+		return null;
+	}
+
+	@Override
+	public Float retencionesAplicadas() {
+		return null;
+	}
+
+	@Override
+	public Float retencionObraSocial() {
+		return null;
+	}
 
 }
--- src/tp_uml/EmpPlantaPermanente.java
+++ src/tp_uml/EmpPlantaPermanente.java
@@ -1,13 +1,33 @@
 package tp_uml;
 
+import java.util.Date;
+
 public class EmpPlantaPermanente extends Empleado {
 
+	private int cantidadHijos;
+	private int antiguedad;
 	
-	
+	public EmpPlantaPermanente(String nombre, String direccion, String estadoCivil, Date fechaNac, Float sueldoBasico,
+			                   int cantHijos, int antiguedad) {
+		super(nombre, direccion, estadoCivil, fechaNac, sueldoBasico);
+		this.cantidadHijos = cantHijos;
+		this.antiguedad = antiguedad;
+	}
+
 	@Override
 	public Float retencionObraSocial() {
 		Float calculo;
 		calculo = this.calculoDeSueldoBruto();
 		return (calculo*10)/100;
+	}
+
+	@Override
+	public Float calculoDeSueldoBruto() {
+		return null;
+	}
+
+	@Override
+	public Float retencionesAplicadas() {
+		return null;
 	}
 }
