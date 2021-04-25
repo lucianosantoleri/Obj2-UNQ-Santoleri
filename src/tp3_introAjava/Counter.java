0cf82dedc190378a1c50b5614e96f184fba25575
Luciano Santoleri <luchosantoleri@gmail.com>
2021-04-25 14:56:42 -0300

    Resolucion hasta punto 3 TP3

--- src/tp3_introAjava/Counter.java
+++ src/tp3_introAjava/Counter.java
@@ -8,8 +8,8 @@ public class Counter {
 
 	private ArrayList<Double> numeros = new ArrayList<Double>();
 	
-	public int numero;
-	public Integer numeroInteger;
+	public static int numero;
+	public static Integer numeroInteger;
 	
 	public Boolean esPar(Double numero) {
 		return (numero%2 == 0);
@@ -42,6 +42,10 @@ public class Counter {
 	public Double getEvenOcurrences() {
 		return (double) soloImpares().size();
 	}
+	
+	public static void main(String[] args) {
+	System.out.println (numeroInteger);
+	}
 }
 
 /* PUNTO 2 - EXAMINAR EXPRESIONES
@@ -66,5 +70,8 @@ public class Counter {
  * 2)- La clase Integer envuelve el tipo primitivo int en un objeto. Un objeto de tipo Integer 
  * contiene un solo campo cuyo valor es de tipo int.
  * 
- * 3)- 
+ * 3)- la variable de instancia de tipo int retorna 0 si no se le asigna ningun valor, la variable de instancia
+ * de la clase de tipo Integer retorna null porque Integer no es un tipo primitivo.
+ * 
+ * 4)- la variable de metodo seria con los mismos retornos que punto anterior.
  * */
