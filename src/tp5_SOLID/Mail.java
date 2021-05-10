970b5adfcd5f6fd034788ba4adf5404bb8634152
Luciano Santoleri <luchosantoleri@gmail.com>
2021-05-10 19:00:27 -0300

    TP5 SOLID

--- src/tp5_SOLID/Mail.java
+++ src/tp5_SOLID/Mail.java
@@ -7,4 +7,4 @@ public class Mail {
 /* /* Claramente una de las violaciones es en esta clase con respecto a la clase ServidorPop en donde no se respeta el Single Responsibility Principle.
  * Una cuenta Mail deberia enviar Correo, y no el servirdor
  * Otro es el Open closed Principle, los atributos deberian estar private para respetar el encapsulamiento, una de las bases de este principio.
- * */
+ *  */
