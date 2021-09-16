61ab6ea2ff0429587842d49333f9f9064ddc1f8c
Luciano Santoleri <luchosantoleri@gmail.com>
2021-09-16 18:43:48 -0300

    nueva actualizacion tp 4

--- tp4_POOenJava/tp4_POOenJava/ProductoPrimeraNecesidad.java
+++ tp4_POOenJava/tp4_POOenJava/ProductoPrimeraNecesidad.java
@@ -2,4 +2,28 @@ package tp4_POOenJava;
 
 public class ProductoPrimeraNecesidad {
 
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	private double valor;
+	
+	public ProductoPrimeraNecesidad(String string, double precio, boolean b) {
+		this.nombre = string;
+		this.valor = precio;
+	}
+
 }
