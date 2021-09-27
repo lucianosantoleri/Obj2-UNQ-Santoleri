7bf392706aaca0afb8a0d010970f3df71d364f87
Luciano Santoleri <luchosantoleri@gmail.com>
2021-09-27 11:13:04 -0300

    avance tp interfaces

---
+++ tp5_AbstractasVSinterfaces/Caja.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+import tp4_POOenJava.Producto;
+
+public class Caja {
+
+	private double montoAPagar;
+	private static ArrayList<Producto> stockProductos = new ArrayList<Producto>();
+	
+	public Caja(double monto, ArrayList<Producto> stock) {
+		this.montoAPagar = monto;
+		this.stockProductos = stock;
+	}
+
+	public double getMontoAPagar() {
+		//this.informarPagoAlCliente(Cliente cliente);
+		return montoAPagar;
+	}
+
+	public void setMontoAPagar(double montoAPagar) {
+		this.montoAPagar = montoAPagar;
+	}
+
+	public static ArrayList<Producto> getStockProductos() {
+		return stockProductos;
+	}
+
+	public static void sacarStockProducto(Producto producto) {
+		if(existeProducto(producto)) {
+			stockProductos.remove(producto);
+			}
+	}
+	
+	public static void aniadirStockProducto(Producto producto) {
+		stockProductos.add(producto);
+	}
+	
+	public static Boolean existeProducto(Producto producto) {
+		return stockProductos.contains(producto);
+	}
+}
