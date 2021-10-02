895867f48229487fe5822d7f37ec49c5d3777fcc
Luciano Santoleri <luchosantoleri@gmail.com>
2021-10-02 13:38:49 -0300

    correccion tp 4

--- tp4_POOenJava/tp4_POOenJava/ProductoCooperativa.java
+++ tp4_POOenJava/tp4_POOenJava/ProductoCooperativa.java
@@ -2,8 +2,12 @@ package tp4_POOenJava;
 
 public class ProductoCooperativa extends Producto {
 
+	public ProductoCooperativa(String nombre, double precio, boolean b) {
+		super(nombre, precio, b);
+	}
+
 	public void aplicarDescuentoIVA() {
-		float nuevoValor = (float) (super.getValor() - (super.getValor() * 0.1));
-		super.setValor(nuevoValor);
+		float nuevoValor = (float) (super.getPrecio() - (super.getPrecio() * 0.1));
+		super.setPrecio(nuevoValor);
 	}
 }
