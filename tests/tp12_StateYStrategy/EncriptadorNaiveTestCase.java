a4492beabae88b6992d0ade8625a87ce2f634ba8
Luciano Santoleri <luchosantoleri@gmail.com>
2021-11-24 18:41:01 -0300

    termino ejercicio

--- tests/tp12_StateYStrategy/EncriptadorNaiveTestCase.java
+++ tests/tp12_StateYStrategy/EncriptadorNaiveTestCase.java
@@ -8,14 +8,18 @@ import org.junit.jupiter.api.Test;
 public class EncriptadorNaiveTestCase {
 
 	private EncriptadorNaive encriptador;
-	private String texto;
+	private EncriptadorNaive encriptador2;
+	public String texto;
+	public String encriptado;
 	
 	@BeforeEach
 	public void setUp() {
 		
 		//sut
 		encriptador = new EncriptadorCambioVocal();
+		encriptador2 = new EncriptadorLetraPorNumero();
 		texto = "hola";
+		encriptado = "hule";
 	}
 	
 	@Test
@@ -24,5 +28,30 @@ public class EncriptadorNaiveTestCase {
 		texto = encriptador.encriptar(texto);
 		
 		assertEquals(texto, "hule");
+	}
+	
+	@Test
+	public void testEncriptandoConNumeros() {
+		
+		texto = encriptador2.encriptar(texto);
+		
+		assertEquals(texto, "h4l1");
+	}
+	
+	@Test
+	public void testDesencriptandoConVocales() {
+		
+		encriptado = encriptador.desencriptar(texto);
+		
+		assertEquals(texto, "hola");
+	}
+	
+	@Test
+	public void testDesencriptandoConNumeros() {
+		
+		texto = "h4l1";
+		encriptado = encriptador2.desencriptar(texto);
+		
+		assertEquals(texto, "hola");
 	}
 }
