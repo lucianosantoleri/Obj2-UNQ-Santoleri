eb46740e20beceba0c2224ed6160486e3308003a
Luciano Santoleri <luchosantoleri@gmail.com>
2021-11-24 23:16:14 -0300

    video Juego

--- tp12_StateYStrategy/tp12_StateYStrategy/IMaquinaVideoJuego.java
+++ tp12_StateYStrategy/tp12_StateYStrategy/IMaquinaVideoJuego.java
@@ -1,6 +1,6 @@
 package tp12_StateYStrategy;
 
 public interface IMaquinaVideoJuego {
-
 	
+	public void controlar(int fichas);
 }
--- tp12_StateYStrategy/tp12_StateYStrategy/VideoJuego.java
+++ tp12_StateYStrategy/tp12_StateYStrategy/VideoJuego.java
@@ -2,5 +2,15 @@ package tp12_StateYStrategy;
 
 public abstract class VideoJuego implements IMaquinaVideoJuego {
 
+	protected int ranura = 2;
+	protected String controles;
 	
+	public VideoJuego(int ranura, String controles) {
+		this.ranura = ranura;
+		this.controles = controles;
+	}
+	
+	public abstract void controlar(int fichas);
+	
+	public abstract void apretarInicio();
 }
