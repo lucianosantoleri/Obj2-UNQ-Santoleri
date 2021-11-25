72849b4292fa001d79ce3169ccee7ea35eebaa1b
Luciano Santoleri <luchosantoleri@gmail.com>
2021-11-25 17:58:38 -0300

    video juego test

--- tests/tp12_StateYStrategy/VideoJuegoTestCase.java
+++ tests/tp12_StateYStrategy/VideoJuegoTestCase.java
@@ -1,5 +1,38 @@
 package tp12_StateYStrategy;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class VideoJuegoTestCase {
 
+	private VideoJuego unJugador;
+	private VideoJuego dosJugadores;
+	public String controles;
+	public int fichas;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		//sut
+		unJugador = new VideoJuegoUnJugador(2, controles);
+		dosJugadores = new VideoJuegoDosJugadores(2, controles);
+		controles = "Arriba,Abajo,Izquierda,Derecha";
+		fichas = 3;
+	}
+	
+	@Test
+	public void testUnJugador() {
+		
+		unJugador.controlar(1);
+		assertEquals(unJugador.ranura, 0);
+	}
+	
+	@Test
+	public void testDosJugadores() {
+		
+		dosJugadores.controlar(2);
+		assertEquals(unJugador.ranura, 2);
+	}
 }
