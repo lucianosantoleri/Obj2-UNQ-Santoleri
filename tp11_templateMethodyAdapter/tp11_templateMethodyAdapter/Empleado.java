0a6367ff78e568d128c056eb7fd83b2455f31a17
Luciano Santoleri <luchosantoleri@gmail.com>
2021-11-28 13:14:17 -0300

    empleado temporario

---
+++ tp11_templateMethodyAdapter/tp11_templateMethodyAdapter/Empleado.java
@@ -0,0 +1,15 @@
+package tp11_templateMethodyAdapter;
+
+public abstract class Empleado {
+
+	protected int horasTrabajo;
+	protected int hijos;
+	
+	public int sueldo() {
+		return this.sueldoBasico();
+	}
+	
+	public abstract int sueldoBasico();
+
+	protected abstract int pagoPorFamilia();
+}
---
+++ tp11_templateMethodyAdapter/tp11_templateMethodyAdapter/Temporario.java
@@ -0,0 +1,43 @@
+package tp11_templateMethodyAdapter;
+
+public class Temporario extends Empleado {
+
+	private boolean estaCasado = false;
+	
+	@Override
+	public int sueldoBasico() {
+		return 1000;
+	}
+	
+	@Override
+	public int sueldo() {
+		return this.sueldoBasico() + this.pagoHorasDeTrabajo() + this.pagoPorFamilia();
+	}
+
+	@Override
+	public int pagoPorFamilia() {
+		if(this.hijosYOCasado()) {
+			return 100;
+		}
+		else {
+		return 0;
+		}
+	}
+	
+	private boolean hijosYOCasado() {
+		return (super.hijos > 0) || this.getEstaCasado() == true;
+	}
+
+	private int pagoHorasDeTrabajo() {
+	
+		return super.horasTrabajo * 5;
+	}
+
+	public boolean getEstaCasado() {
+		return estaCasado;
+	}
+
+	public void setEstaCasado(boolean estaCasado) {
+		this.estaCasado = estaCasado;
+	}
+}
