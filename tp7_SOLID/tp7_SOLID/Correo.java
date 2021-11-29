2dea29b03569b946818d091cd4437916c52a0f38
Luciano Santoleri <luchosantoleri@gmail.com>
2021-11-29 12:06:38 -0300

    tp solid

--- tp7_SOLID/tp7_SOLID/Correo.java
+++ tp7_SOLID/tp7_SOLID/Correo.java
@@ -4,7 +4,6 @@ package tp7_SOLID;
 public class Correo {
 
 	public Correo(String asunto, String destinatario, String cuerpo) {
-		// TODO Auto-generated constructor stub
+		System.out.println("Para: " + destinatario + ". Asunto: " + asunto + ". Cuerpo: " + cuerpo);
 	}
-
 }
